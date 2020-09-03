1fbc6fc104f9e4d65d9028c0a1f1584184889a72
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-03 21:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ str=tutorials.map(setc => setc.toLowerCase().split(' '));
     for (var j=0; j< str[i].length; j++){
       str[i][j] = str[i][j].charAt(0).toUpperCase() + str[i][j].slice(1); 
     }
+    str[i].join(' ')
   }
   return str;
 }
