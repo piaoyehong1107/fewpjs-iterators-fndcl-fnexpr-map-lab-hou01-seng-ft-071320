e4460ac6f4bfb6fc3b23a5601c6f79dcb7c7a36e
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-03 21:44:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-str=tutorials.map(setc => setc.toLowerCase().split(' '));
+const str=tutorials.map(setc => setc.toLowerCase().split(' '));
   for (var i = 0; i < str.length; i++) {
     for (var j=0; j< str[i].length; j++){
       str[i][j] = str[i][j].charAt(0).toUpperCase() + str[i][j].slice(1); 
